ebc579083a6e3a1522fbceeb527289daf108327f
Jeff Blanchette <jeff.blanchette5@gmail.com>
2020-07-16 18:33:55 -0400

    two tests

--- bug_test/main.js
+++ bug_test/main.js
@@ -1,13 +1,25 @@
 const wc3v         = require('../wc3v');
-const replayFile = '../replays/bufferissue.w3g';
 
-console.log("starting bug test");
+const replayPath1 = '../replays/bufferissue.w3g';
+
+console.log("starting bug test 1");
 
 const results = wc3v.parseReplays({
         inTestMode: true,
         isProduction: true,
-        paths: [replayPath],
-        hashes: [replayHash]
+        paths: [replayPath1]
       }); 
 
-console.log("done with bug test");
+console.log("done with bug test 1");
+
+const replayPath2 = '../replays/bufferissue.w3g';
+
+console.log("starting bug test 2");
+
+const results = wc3v.parseReplays({
+        inTestMode: true,
+        isProduction: true,
+        paths: [replayPath2]
+      }); 
+
+console.log("done with bug test 2");
