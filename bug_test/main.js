8b34156277e386120bde9e83e6fc145d47445cee
Jeff Blanchette <jeff.blanchette5@gmail.com>
2020-07-16 18:32:06 -0400

    test 2

--- bug_test/main.js
+++ bug_test/main.js
@@ -1,15 +1,13 @@
-const W3GReplay = require('../node_modules/w3gjs');
-const Parser = new W3GReplay();
-
+const wc3v         = require('../wc3v');
 const replayFile = '../replays/bufferissue.w3g';
 
 console.log("starting bug test");
 
-try {
-  Parser.parse(replayFile);
-  console.log("replay worked");
-} catch (e) {
-  console.log("error: ", e);
-}
+const results = wc3v.parseReplays({
+        inTestMode: true,
+        isProduction: true,
+        paths: [replayPath],
+        hashes: [replayHash]
+      }); 
 
 console.log("done with bug test");
